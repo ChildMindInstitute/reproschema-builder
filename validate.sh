f80904046042fd866f1a823b85569c6c4b9fd5c8
charlie42 <oninfrared@gmail.com>
2020-12-16 11:19:07 +0100

    test with less qrs (38 is last)

---
+++ validate.sh
@@ -0,0 +1,81 @@
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr10/qr10_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr12/qr12_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr13/qr13_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr14/qr14_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr15/qr15_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr16/qr16_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr17/qr17_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr18/qr18_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr19/qr19_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr20/qr20_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr21/qr21_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr23/qr23_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr24/qr24_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr25/qr25_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr26/qr26_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr27/qr27_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr30/qr30_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr31/qr31_schema
+#
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr32/qr32_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr34/qr34_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr36/qr36_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr37/qr37_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr38/qr38_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr39/qr39_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr40/qr40_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr41/qr41_schema
+#
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr42/qr42_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr43/qr43_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr45/qr45_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr46/qr46_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr48/qr48_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr49/qr49_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr50/qr50_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr51/qr51_schema
+#
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr53/qr53_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr54/qr54_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr56/qr56_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr58/qr58_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr60/qr60_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr61/qr61_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr62/qr62_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr63/qr63_schema
+#
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr64/qr64_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr65/qr65_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr68/qr68_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr69/qr69_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr71/qr71_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr72/qr72_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr73/qr73_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr74/qr74_schema
+#
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr75/qr75_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr77/qr77_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr80/qr80_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr81/qr81_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr87/qr87_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr88/qr88_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr89/qr89_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr91/qr91_schema
+#
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr92/qr92_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr93/qr93_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr94/qr94_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr95/qr95_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr96/qr96_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr97/qr97_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr98/qr98_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr102/qr102_schema
+#
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr107/qr107_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr108/qr108_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr109/qr109_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr110/qr110_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr111/qr111_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr112/qr112_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr113/qr113_schema
+reproschema validate /Users/charlie/Documents/projects/cmi/phd/reproschema-builder/activities/qr115/qr115_schema
