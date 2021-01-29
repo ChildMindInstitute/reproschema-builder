4e1b593c87febe33d46649cda42f16423a065fa3
charlie42 <oninfrared@gmail.com>
2021-01-29 18:57:15 +0100

    fix branch and protocol name in script

--- tools/RedCap2ReproSchema.js
+++ tools/RedCap2ReproSchema.js
@@ -4,12 +4,12 @@
 const protocolName = "sc_dd";
 
 //2. your protocol display name: this will show up in the app and be parsed as a string
-const protocolDisplayName = "LISA";
+const protocolDisplayName = "LISA META";
 
 //2. create your raw github repo URL
 const userName = 'charlie42';
 const repoName = 'reproschema-builder';
-const branchName = 'ifea';
+const branchName = 'ifea-meta';
 
 let yourRepoURL = `https://raw.githubusercontent.com/${userName}/${repoName}/${branchName}`;
 
